9b89ad9739608cfdca1d85694fd10cd0124701ed
Kenji Rikitake <kenji.rikitake@acm.org>
2015-01-22 10:00:14 +0900

    Add src/emprng.erl

---
+++ src/emprng.erl
@@ -0,0 +1,137 @@
+%% Copyright 2015 Kenji Rikitake.
+%% 
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(emprng).
+
+-export([seed/0, seed/1, seed/2, seed/3, seed0/0, seed0/1,
+         uniform/0, uniform/1, uniform_s/1, uniform_s/2]).
+
+-define(DEFAULT_ALG_HANDLER, emprng_as183).
+-define(SEED_DICT, emprng_seed).
+
+%% Types
+
+%% This depends on the algorithm handler module
+-type emprng_alg_state() :: any().
+%% This is the name of the algorithm handler module
+-type emprng_alg_handler() :: atom().
+%% Internal state
+-type emprng_state() :: {emprng_alg_handler(), emprng_alg_state()}.
+
+%%% Note: if a process calls uniform/0 or uniform/1 without setting a seed first,
+%%%       seed/0 is called automatically.
+%%% (compatible with the random module)
+
+%% seed0/0: returns the default state, including the state values
+%% and the algorithm handler.
+%% (compatible with the random module)
+
+-spec seed0() -> emprng_state().
+
+seed0() ->
+    seed0(?DEFAULT_ALG_HANDLER).
+
+%% seed0/1: returns the default state for the given algorithm handler.
+%% (new function)
+
+-spec seed0(emprng_alg_handler()) -> emprng_state().
+
+seed0(Alg) ->
+    {Alg, Alg:seed0()}.
+
+%% seed_put/1: internal function to put seed into the process dictionary.
+
+-spec seed_put(emprng_state()) -> undefined | emprng_state().
+
+seed_put(Seed) ->
+    put(?SEED_DICT, Seed).
+
+%% seed/0: seeds RNG with default (fixed) state values and the algorithm handler
+%% in the process dictionary, and returns the old state.
+%% (compatible with the random module)
+
+-spec seed() -> undefined | emprng_state().
+
+seed() ->
+    case seed_put(seed0()) of
+        undefined -> seed0();
+        % no type checking here
+        Old -> Old
+    end.
+
+%% seed/1: seed({A1, A2, A3}) is equivalent to seed(A2, A2, A3).
+%% (compatible with the random module)
+
+-spec seed({A1 :: integer(), A2 :: integer(), A3 :: integer()}) ->
+      undefined | emprng_state().
+
+seed({A1, A2, A3}) ->
+    seed(A1, A2, A3).
+
+%% seed/3: seeds RNG with integer values in the process dictionary,
+%% and returns the old state.
+%% (compatible with the random module)
+
+-spec seed(A1 :: integer(), A2 :: integer(), A3 :: integer()) ->
+      undefined | emprng_state().
+
+seed(A1, A2, A3) ->
+    seed_put({?DEFAULT_ALG_HANDLER,
+            ?DEFAULT_ALG_HANDLER:seed(A1, A2, A3)}).
+
+%% seed/2: seeds RNG with the given values and algorithm handler
+%% in the process dictionary, and returns the old state.
+%% Note: the type of the values depends on the algorithm handler.
+%% (new function)
+
+-spec seed(X :: emprng_alg_state(), Alg :: emprng_alg_handler()) ->
+      undefined | emprng_state().
+
+seed(X, Alg) ->
+    seed_put({?DEFAULT_ALG_HANDLER,
+            % No type checking on X
+             X}).
+
+%%% uniform/0, uniform/1, uniform_s/1, uniform_s/2 are all
+%%% uniformly distributed random numbers.
+%%% (They are expected to be semantically compatible with OTP random module,
+%%%  i.e., the uniformity and return value ranges will not change.)
+
+%% uniform/0: returns a random float X where 0.0 < X < 1.0,
+%% updating the state in the process dictionary.
+%% (See OTP random module.)
+
+-spec uniform() -> float().
+
+%% uniform/1: given an integer N >= 1,
+%% uniform/1 returns a random integer X where 1 =< X =< N,
+%% updating the state in the process dictionary.
+
+-spec uniform(N :: pos_integer()) -> pos_integer().
+
+%% uniform_s/1: given a state, uniform_s/1
+%% returns a random float X where 0.0 < X < 1.0,
+%% and a new state.
+%% (See OTP random module.)
+
+-spec uniform_s(S :: emprng_state()) -> {float(), NewS :: emprng_state()}.
+
+%% uniform_s/2: given an integer N >= 1 and a state, uniform_s/2
+%% uniform_s/2 returns a random integer X where 1 =< X =< N,
+%% and a new state.
+
+-spec uniform_s(N :: pos_integer(), S :: emprng_state()) ->
+      {pos_integer(), NewS :: emprng_state()}.
+
+
